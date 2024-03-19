17fb45bcceaa33f6fcd2b0e462cbe373f9419335
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-03-20 00:58:24 +0530

    Added Components

--- Client/react-swc-ts/src/App.tsx
+++ Client/react-swc-ts/src/App.tsx
@@ -6,9 +6,9 @@ function App() {
     <>
       <div className="absolute inset-0 -z-10 h-full w-full brightness-90 bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
       <div className="h-lvh flex justify-center items-center ">
-        <div className=' backdrop-blur-sm bg-transparent h-28 w-48 flex justify-center items-center'>
-          <div className='bg-white/35 backdrop-blur-lg m-3 p-3'>
-            <h1 className=' font-hachi font-bold text-pink-600 text-6xl'>
+        <div className=' backdrop-blur-sm bg-transparent h-36 w-96 flex justify-center items-center'>
+          <div className='m-3 p-3 bg-black/35 backdrop-blur-lg'>
+            <h1 className=' font-hachi font-bold text-pink-400 text-6xl'>
               SHUBHANKAR KAUSHIK
             </h1>
           </div>
